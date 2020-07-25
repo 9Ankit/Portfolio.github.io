96205dce50883ecd0810016ff26898a0dd7bdcad
Ankit Gupta <gua9838@gmail.com>
2020-07-25 23:42:21 +0530

    make all componets, modules, variable.scss, style.scss -- Structure Complete

--- MyPortfolio/src/app/app.module.ts
+++ MyPortfolio/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { PageNotFoundComponent } from './layout/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule
