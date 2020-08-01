11d9cb432d98daac0caee2ea2103c7d932320208
Ankit Gupta <gua9838@gmail.com>
2020-08-01 21:52:29 +0530

    add fxFlex in module.ts

--- MyPortfolio/src/app/app.module.ts
+++ MyPortfolio/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { PageNotFoundComponent } from './layout/page-not-found/page-not-found.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+// import { AboutMeComponent } from './feature/about-me/about-me.component';
+// import { FeatureModule } from './feature/feature.module';
 
 @NgModule({
   declarations: [
@@ -12,8 +15,13 @@ import { PageNotFoundComponent } from './layout/page-not-found/page-not-found.co
     HeaderComponent,
     SidebarComponent,
     PageNotFoundComponent,
+    // AboutMeComponent,
   ],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    FlexLayoutModule,
+    //  FeatureModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
